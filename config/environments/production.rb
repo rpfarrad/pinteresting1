89f8f75c36cda1e3faed108d22c7d8ba724f3439
rpfarrad <rpfarrad@yahoo.com>
2014-06-05 23:11:57 -0400

    Add amazon s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,14 @@ Pinteresting::Application.configure do
   #Note that I have to set this with the host name
   config.action_mailer.default_url_options = { host: 'http://pinteresting-commits.herokuapp.com/' }
 
+  #Sets Paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}  
+
 end
